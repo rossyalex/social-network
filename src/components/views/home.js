337c9db208ba0ef3305c5251b0abf7259f769243
Camila-TS <camilathompson@gmail.com>
2022-08-24 12:42:44 -0400

    creación de vistas home, feeding, training

--- src/components/views/home.js
+++ src/components/views/home.js
@@ -1,5 +1,10 @@
 export const home = () => {
     
-    return `<h1>Home</h1>
-            <p>Esto es solo para probar...</p>`;
+    return `<h1>Runner Lovers</h1>
+            <p>Acá podrás revisar y compartir todo la información actualizada de nuestra pasión, además de acompañarnos en los entrenamientos.</p>
+            <label for="comment"></label>
+            <input class="" type="text">
+                <a><i class="fa-solid fa-hand-holding-heart"></i></a>
+                <a><i class="fa-solid fa-comment"></i></a>`;
+            
 }
--- src/components/views/training.js
+++ src/components/views/training.js
@@ -1,4 +1,8 @@
 export const training = () => {
   return `<h1>Entrenamientos y Eventos</h1>
-            <p>Esto es solo para probar...</p>`;
+            <p>Espacio dedicado para compartirnos información relacionada a rutinas de entrenamiento, formar grupos de entrenamiento en conjunto y más</p>
+            <label for="comment"></label>
+            <input class="" type="text">
+                <a><i class="fa-solid fa-hand-holding-heart"></i></a>
+                <a><i class="fa-solid fa-comment"></i></a>`;
 }
