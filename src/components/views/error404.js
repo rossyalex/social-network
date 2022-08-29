81f2dcf6aa6b3481ba804c335dbd4e363e818555
Rossy Perez <rossyesmeral@gmail.com>
2022-08-29 01:09:16 -0400

    router arreglado

--- src/components/views/error404.js
+++ src/components/views/error404.js
@@ -1,8 +1,4 @@
-export const notFound = () => {
-  const root = document.getElementById('root');
-  const notFound = `<div style="text-align: center">
+export const notFound = () => `<div style="text-align: center">
               <h1>404 Not Found</h1>
               <p>Oh no! It looks like the page you're trying to get to is missing!</p>
             </div>`;
-  root.innerHTML = notFound;
-};
