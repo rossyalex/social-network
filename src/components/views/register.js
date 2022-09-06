d3b08788ef07ff3ba5b16fe8720a201b2def81d0
Rossy Perez <rossyesmeral@gmail.com>
2022-09-06 08:28:02 -0400

    borrando value

--- src/components/views/register.js
+++ src/components/views/register.js
@@ -4,14 +4,14 @@ export const register = () => `<div class="card"><img id="logoRunners" src="../.
     <h1>Crear Usuario</h1>
     <form class="buttonsWrapper" action="" method="post">
           <label for="name">Nombre</label>
-          <input  id="name" class="user" type="text" placeholder="Ingresa tu Nombre" value="Yick">
+          <input  id="name" class="user" type="text" placeholder="Ingresa tu Nombre">
           <label for="email">Correo electrónico</label>
-          <input  id="email" class="user" type="text" placeholder="Ingresa tu email" value="email@gmail.com">
+          <input  id="email" class="user" type="text" placeholder="Ingresa tu email">
           <label for="password">Contraseña</label>
-          <input id="password" class="user" type="password" placeholder="********" value="123">
+          <input id="password" class="user" type="password" placeholder="********">
           <i class="far fa-eye passwordEye"></i><br>
           <label id="textRepeatPassword" for="repeatpassword">Repetir contraseña</label>
-          <input id="repeatPassword" class="user" type="password" placeholder="********" value="123">
+          <input id="repeatPassword" class="user" type="password" placeholder="********">
           <i class="far fa-eye passwordEye"></i>
           <p id="errorInfo" class="error-message hide">Verifique sus datos y vuelva a intentar</p>
           <p id="errorUserCreated" class="error-message hide">Usuario ya registrado</p>
