a067710792ad7f846ccfa263bd77a9804add40f9
CataGarciaRojas <cattapgarcia@gmail.com>
2022-09-09 13:19:02 -0300

    Modificamos Logo y estilos de letras

--- src/components/views/signin.js
+++ src/components/views/signin.js
@@ -1,7 +1,6 @@
 /* eslint-disable eol-last */
 
-export const signin = () => `<div class="card"> <img id="logoRunners" src="../../img/logo1.png" alt="Logo applicación">
-        <h1>Runner Lovers</h1>
+export const signin = () => `<div class="card"> <img id="logoRunners" src="../../img/logo1.png" alt="Logo applicación"><br><br>
         <div class="container-buttons">
           <button id="initiation" type="button" class="button"><span>Iniciar Sesión</span></button>
           <button id="create" type="button" class="button"><span>Crear Cuenta</span></button>
