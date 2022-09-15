bb73d9ec49b68bab62009a01b4a19d15adf2bb7c
Camila-TS <camilathompson@gmail.com>
2022-09-15 09:54:11 -0300

    trabajamos en las vistas de feeding y training

--- src/components/views/training.js
+++ src/components/views/training.js
@@ -1,11 +1,10 @@
 /* eslint-disable eol-last */
 
-export const training = () => `<div class="card"> 
+export const training = () => `<div id="training" class="card"> 
             <h1>Entrenamientos y Eventos</h1>
             <p>Espacio dedicado para compartirnos información relacionada a rutinas de entrenamiento, formar grupos de entrenamiento en conjunto y más</p>
             <label for="comment"></label>
             <textarea name="content" id="trainingContent" cols="35" rows="10"></textarea>
-                <a><i class="fa-solid fa-hand-holding-heart"></i></a>
-                <a><i class="fa-solid fa-comment"></i></a>
-                <a><i class="fa-solid fa-trash-can"></i></a>
-                </div>`;
+            <button id="createPost" class="button"><span><i class="fa-solid fa-paper-plane"></i></span></button>
+        </div>
+        <div id="renderPosts"></div>`;
