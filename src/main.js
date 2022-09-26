bfa199161c66648945707bd5a59a04437fa86e6c
Rossy Perez <rossyesmeral@gmail.com>
2022-09-25 22:47:15 -0300

    Change minor

--- src/main.js
+++ src/main.js
@@ -1,9 +1,3 @@
-/* // Este es el punto de entrada de tu aplicacion
-
-import { myFunction } from './lib/index.js';
-
-myFunction(); */
-
 import { handleLocation, route } from './js/router.js';
 
 window.onpopstate = handleLocation;
